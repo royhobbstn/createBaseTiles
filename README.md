7270efc1194304c5aa24984b17e8e42537adef1e
Daniel Trone <danieljtrone@gmail.com>
2018-02-27 02:40:46 +0000

    rough outline

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # createBaseTiles
 Create a simplistic basemap using Natural Earth data and tippecanoe.
+
+## Setup
+
+```
+bash downloadGeo.sh
+node convertToGeoJSON.js
+node transformGeoJSON.js
+tippecanoe something something something
+```
+
---
+++ downloadGeo.sh
@@ -0,0 +1,28 @@
+rm -rf shapefiles
+mkdir shapefiles
+cd shapefiles
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries_lakes.zip
+unzip ne_10m_admin_0_countries_lakes.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_boundary_lines_land.zip
+unzip ne_10m_admin_0_boundary_lines_land.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces_lines.zip
+unzip ne_10m_admin_1_states_provinces_lines.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_label_points.zip
+unzip ne_10m_admin_1_label_points.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_parks_and_protected_lands.zip
+unzip ne_10m_parks_and_protected_lands.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip
+unzip ne_10m_populated_places_simple.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_roads_north_america.zip
+unzip ne_10m_roads_north_america.zip
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_railroads_north_america.zip
+unzip ne_10m_railroads_north_america.zip
+
